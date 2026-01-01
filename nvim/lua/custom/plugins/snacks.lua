6b460d47e1fcf3f36aba844e609d47f14b500d52
Emre Sin <emre.sin@codeway.co>
2026-01-01 08:57:35 +0300

    migrated from telescope to snacks.picker, replaced lsp renaming with snacks.rename

--- nvim/lua/custom/plugins/snacks.lua
+++ nvim/lua/custom/plugins/snacks.lua
@@ -14,6 +14,19 @@ return {
       quickfile = { enabled = true },
       statuscolumn = { enabled = true },
       words = { enabled = true },
+      picker = {
+        enabled = true,
+        ui_select = true, -- replace vim.ui.select with snacks picker
+        win = {
+          input = {
+            keys = {
+              -- Remap C-u/C-d to scroll preview instead of list
+              ['<C-u>'] = { 'preview_scroll_up', mode = { 'i', 'n' } },
+              ['<C-d>'] = { 'preview_scroll_down', mode = { 'i', 'n' } },
+            },
+          },
+        },
+      },
       styles = {
         notification = {
           wo = { wrap = true }, -- Wrap notifications
@@ -21,6 +34,7 @@ return {
       },
     },
     keys = {
+      -- Existing keymaps
       {
         '<leader>gg',
         function()
@@ -35,6 +49,121 @@ return {
           Snacks.terminal.toggle()
         end,
         desc = 'Toggle Terminal',
+      },
+      -- Picker keymaps (migrated from telescope)
+      {
+        '<leader>sh',
+        function()
+          Snacks.picker.help()
+        end,
+        desc = '[S]earch [H]elp',
+      },
+      {
+        '<leader>sk',
+        function()
+          Snacks.picker.keymaps()
+        end,
+        desc = '[S]earch [K]eymaps',
+      },
+      {
+        '<C-p>',
+        function()
+          Snacks.picker.files()
+        end,
+        desc = '[S]earch [F]iles',
+      },
+      {
+        '<leader>ss',
+        function()
+          Snacks.picker.lsp_workspace_symbols()
+        end,
+        desc = '[S]earch Workspace [S]ymbols',
+      },
+      {
+        '<leader>sw',
+        function()
+          Snacks.picker.grep_word()
+        end,
+        desc = '[S]earch current [W]ord',
+        mode = { 'n', 'x' },
+      },
+      {
+        '<leader>sg',
+        function()
+          Snacks.picker.grep()
+        end,
+        desc = '[S]earch by [G]rep',
+      },
+      {
+        '<leader>s.',
+        function()
+          Snacks.picker.recent()
+        end,
+        desc = '[S]earch Recent Files ("." for repeat)',
+      },
+      {
+        '<leader><leader>',
+        function()
+          Snacks.picker.buffers()
+        end,
+        desc = '[ ] Find existing buffers',
+      },
+      {
+        '<leader>/',
+        function()
+          Snacks.picker.lines()
+        end,
+        desc = '[/] Fuzzily search in current buffer',
+      },
+      {
+        '<leader>s/',
+        function()
+          Snacks.picker.grep_buffers()
+        end,
+        desc = '[S]earch [/] in Open Files',
+      },
+      {
+        '<leader>sn',
+        function()
+          Snacks.picker.files { cwd = vim.fn.stdpath 'config' }
+        end,
+        desc = '[S]earch [N]eovim files',
+      },
+      -- Git
+      {
+        '<leader>gf',
+        function()
+          Snacks.picker.git_log_file()
+        end,
+        desc = 'Git log for current file',
+      },
+      {
+        '<leader>gd',
+        function()
+          Snacks.picker.git_diff()
+        end,
+        desc = 'Git log for current file',
+      },
+      {
+        '<leader>gb',
+        function()
+          Snacks.picker.git_branches()
+        end,
+        desc = 'Git log for current file',
+      },
+      {
+        '<leader>gl',
+        function()
+          Snacks.picker.git_log()
+        end,
+        desc = 'Git log for current file',
+      },
+      {
+        '<leader>gS',
+        function()
+          Snacks.picker.git_stash()
+        end,
+        desc = 'Git log for current file',
       },
     },
     init = function()
--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -13,13 +13,26 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
-  opts = {
-    filesystem = {
+  lazy = false,
+  opts = function(_, opts)
+    local function on_move(data)
+      Snacks.rename.on_rename_file(data.source, data.destination)
+    end
+    local events = require 'neo-tree.events'
+    opts.event_handlers = opts.event_handlers or {}
+    vim.list_extend(opts.event_handlers, {
+      { event = events.FILE_MOVED, handler = on_move },
+      { event = events.FILE_RENAMED, handler = on_move },
+    })
+    opts.filesystem = {
+      -- hijack_netrw_behavior = 'open_default',
+      -- use_libuv_file_watcher = true,
       window = {
         mappings = {
           ['\\'] = 'close_window',
         },
       },
-    },
-  },
+    }
+    require('neo-tree').setup(opts)
+  end,
 }
